59b8d40144c1253e9ebbbfe14e70c887dd0859ee
Chaim Reisman <crreisman@gmail.com>
2018-03-21 19:29:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,5 +20,17 @@ class App < Sinatra::Base
   
   get "/say/:word1/:word2/:word3/:word4/:word5"
     @w1 = params[:word1]
+    @w2 = params[:word2]
+    @w3 = params[:word3]
+    @w4 = params[:word4]
+    @w5 = params[:word5]
+    "#{@w1 @w2 @w3 @w4 @w5}"
+  end 
+  
+  get "/:operation/:number1/:number2"
+    @op = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    "#{@num1 @op @num2}"
   end 
 end
